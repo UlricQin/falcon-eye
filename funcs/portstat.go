e8dfeeaa18381ae2ddecc1a61b21b9607045adaf
qinxiaohui [秦晓辉] <qinxiaohui@xiaomi.com>
2014-06-18 17:36:24 +0800

    bugfix

--- funcs/portstat.go
+++ funcs/portstat.go
@@ -6,16 +6,5 @@ import (
 )
 
 func PortIsListen(port int64) bool {
-	tcp := collector.ListenPorts("tcp")
-	tcp6 := collector.ListenPorts("tcp6")
-	udp := collector.ListenPorts("udp")
-	udp6 := collector.ListenPorts("udp6")
-
-	if slicetool.SliceContainsInt64(tcp, port) ||
-		slicetool.SliceContainsInt64(tcp6, port) ||
-		slicetool.SliceContainsInt64(udp, port) ||
-		slicetool.SliceContainsInt64(udp6, port) {
-		return true
-	}
-	return false
+    return slicetool.SliceContainsInt64(collector.ListenPorts(), port)
 }
